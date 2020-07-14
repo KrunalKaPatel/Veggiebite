dbb9d6c4229d0730e4f9551aae49de007fe6575f
Krunalkumar Patel <INT019@uplers.local>
2020-07-14 18:24:47 +0530

    Initial Project

---
+++ assets/js/general.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	/* Slider for Section*/
+	$('.banner-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrow: false,
+		dots: false
+	});
+});
