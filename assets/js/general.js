b5027320b5a24d6b631fe43a0b0431e15c699d80
Krunalkumar Patel <INT019@uplers.local>
2020-07-22 14:15:35 +0530

    Final Update

--- assets/js/general.js
+++ assets/js/general.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
 		arrow: false,
 		dots: false
 	});
+
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: false,
+	  asNavFor: '.slider-nav'
+	});
+
+	$('.slider-nav').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  centerMode: false,
+	  focusOnSelect: false
+	});
 });
